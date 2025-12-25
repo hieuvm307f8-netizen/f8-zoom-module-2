71a17b7712815ac8d7241e709d4adda2ad2ffb33
hieuvm307f8-netizen <hieuvm307f8@fullstack.edu.vn>
2025-12-25 22:04:44 +0700

    fix url image

--- src/components/header.js
+++ src/components/header.js
@@ -26,7 +26,7 @@ export default function header() {
                 <div class="actions">
                     ${isLogin ? `
                         <div class="user-logged" style="display: flex; align-items: center; gap: 10px;">
-                            <img src="/public/images/no-avatar-350x350-1.jpeg" alt="Avatar" class="user-avatar" style="width: 35px; border-radius: 50%;" />
+                            <i class="fa-solid fa-user user-avatar" alt="Avatar" style="font-size: 35px; border-radius: 50%; color: #fff"></i>
                             <button id="logout-btn" class="btn-logout">
                                 Đăng xuất <i class="fa-solid fa-arrow-right-from-bracket"></i>
                             </button>
